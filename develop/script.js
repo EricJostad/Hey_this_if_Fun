7c011c9a1775e3e917d7c4c568b033304004f577
EJostad <eric.jostad@outlook.com>
2021-07-25 20:14:20 -0700

    Added code too JS file that will allow for user input to controlthe white box.

--- develop/script.js
+++ develop/script.js
@@ -1,1 +1,11 @@
-var number1 = "Wow, I am the first variable of this program. But i'm also just a string...do better!"
+let movement = 10; 
+
+window.addEventListener('keydown', (event) => {
+    const {style} = block;
+    switch (event.key) {
+        case 'ArrowUp': style.top = `${parseInt(style.top) - movement}px`; break;
+        case 'ArrowDown': style.top = `${parseInt(style.top) + movement}px`; break;
+        case 'ArrowLeft': style.left = `${parseInt(style.left) - movement}px`; break;
+        case 'ArrowRight': style.left = `${parseInt(style.left) + movement}px`; break;
+    }
+});
