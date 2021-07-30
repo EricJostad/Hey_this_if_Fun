d5ea43b84ba5918e2e6b2dd6d5a728c9c62e7b65
EJostad <eric.jostad@outlook.com>
2021-07-29 23:14:56 -0700

    Refactored script.js slightly.

--- develop/script.js
+++ develop/script.js
@@ -1,27 +1,27 @@
 let box = document.querySelector('#box');
-let movement = 10; 
+let movement = 10;
 
 
 window.addEventListener('load', () => {
     box.style.position = 'absolute';
     box.style.left = '10';
-    box.style.top= '10';
+    box.style.top = '10';
 });
 
 
 window.addEventListener('keyup', (event) => {
     switch (event.key) {
-        case 'ArrowUp': 
-        box.style.top = parseInt(box.style.top) - movement + 'px'; 
+        case 'ArrowLeft':
+            box.style.left = parseInt(box.style.left) - movement + 'px';
             break;
-        case 'ArrowDown': 
-        box.style.top = parseInt(box.style.top) + movement + 'px'; 
+        case 'ArrowRight':
+            box.style.left = parseInt(box.style.left) + movement + 'px';
             break;
-        case 'ArrowLeft': 
-        box.style.left = parseInt(box.style.top) - movement + 'px'; 
+        case 'ArrowUp':
+            box.style.top = parseInt(box.style.top) - movement + 'px';
             break;
-        case 'ArrowRight': 
-        box.style.left = parseInt(box.style.top) + movement + 'px'; 
+        case 'ArrowDown':
+            box.style.top = parseInt(box.style.top) + movement + 'px';
             break;
     }
 });
