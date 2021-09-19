1b276cee28e401370cfe0ee3636aadd93f39acb8
EJostad <eric.jostad@outlook.com>
2021-09-19 10:57:43 -0700

    Moved box-control logic to load function. Now working on stopping code from running if Cancel is chosen.

--- develop/script.js
+++ develop/script.js
@@ -1,33 +1,44 @@
 let box = document.querySelector('.box');
 let movement = 5;
-let isExecuted = confirm("Are you sure to execute this action?")
+let isExecuted = confirm('Hello! Should we load the page?')
 
-window.addEventListener('load', () => {
-    box.style.position = 'absolute';
-    box.style.left = 0;
-    box.style.top = 0;
-    console.log('Event listener for load hit and ran.')
-});
+function load(isExecuted){
+    if (isExecuted = true) {
 
-
-window.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'ArrowLeft':
-            box.style.left = parseInt(box.style.left) - movement + 'px';
-            break;
-        case 'ArrowRight':
-            box.style.left = parseInt(box.style.left) + movement + 'px';
-            break;
-        case 'ArrowUp':
-            box.style.top = parseInt(box.style.top) - movement + 'px';
-            break;
-        case 'ArrowDown':
-            box.style.top = parseInt(box.style.top) + movement + 'px';
-            break;
-    console.log('I ran!')
+        window.addEventListener('load', () => {
+            box.style.position = 'absolute';
+            box.style.left = 0;
+            box.style.top = 0;
+            console.log('Event listener for load hit and ran.')
+        });
+        
+        
+        window.addEventListener('keydown', (event) => {
+            switch (event.key) {
+                case 'ArrowLeft':
+                    box.style.left = parseInt(box.style.left) - movement + 'px';
+                    break;
+                case 'ArrowRight':
+                    box.style.left = parseInt(box.style.left) + movement + 'px';
+                    break;
+                case 'ArrowUp':
+                    box.style.top = parseInt(box.style.top) - movement + 'px';
+                    break;
+                case 'ArrowDown':
+                    box.style.top = parseInt(box.style.top) + movement + 'px';
+                    break;
+            console.log('I ran!')
+            }
+            console.log('Event listener for keyup hit and ran.')
+        });
+        
+        
+    } else {
+        console.log('Page was not loaded.')
     }
-    console.log('Event listener for keyup hit and ran.')
-});
+}
+
+load()
 
 function test(){
 
